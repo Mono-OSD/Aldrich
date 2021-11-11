7947ed130710b6b0608ed27f4e91dc8718c17a55
MrWinterz98 <anonymousdabido@gmail.com>
2021-11-11 19:17:01 +1100

    Added majour changes, including new cmds and fixes

--- laboratory/sudo2.py
+++ laboratory/sudo2.py
@@ -1,3 +1,30 @@
 """
 Sudo2 is for Loomgild.py
 """
+import time
+from time import sleep
+import loomgild
+
+# Command Functions
+
+def help():
+    print("Hello!")
+    print("Welcome to Loomgild, a script that imitates a command line.")
+    print("This is one of the few commands that you can use.")
+    print("We will now load the commands you can use..")
+    print("\n")
+    sleep(2.00)
+    print("Sys commands: exit")
+    print("Core commands: none")
+    print("Utility commands: help")
+    print("Misc commands: none")
+
+def output():
+    prompt1 = input("Please provide the input to output: ")
+    print(prompt1)
+
+def output_h():
+    if loomgild.cmd == "output -h":
+        print("Description: A command that outputs the user's input.")
+        print("Usage: output")
+        print("Tags: input, interactive")
--- loomgild.py
+++ loomgild.py
@@ -1,5 +1,58 @@
-print("Welcome")
-print("This will be maintained soon")
-print("Stay tuned")
+"""
+Some parts are commented just for testing purposes.
+On post-production, these comments will be removed or reduced.
 
-print("I rushed this so I can't really do it.")
+By parts, I mean the sleep() functions.
+"""
+import time
+from time import sleep
+import random
+import laboratory
+from laboratory import sudo2
+import sys
+
+print("Welcome!")
+print("Loomgild is a project that tries to imitate a command-line!")
+print("Please wait for it to initialise..")
+
+#sleep(2.00)
+
+print("---")
+sleep(0.50)
+print("---")
+sleep(0.50)
+print("---")
+sleep(0.50)
+
+print("Starting initialising process.. (1/3)")
+#sleep(4.0)
+print("Installing necessary modules.. (2/3)")
+#sleep(7.0)
+print("Downloading necessary packages.. (3/3)")
+print("- Package 1: SudoLabo (:: Downloading)")
+#sleep(6.9)
+print("- SudoLabo (:: Installing..)")
+#sleep(22.0)
+print("- SudoLabo (:: Cleaning things up..)")
+#sleep(17.0)
+print("Rebooting..")
+#sleep(6.0)
+
+print("Loomgild is a project that tries to imitate a command-line!")
+print("Type in the command line to get started!")
+
+cmd = input("> ")
+
+while cmd == len(cmd)==2 or 3:
+    cmd = input("> ")
+
+    if cmd == "help":
+        sudo2.help()
+    if cmd == "exit":
+        sys.exit()
+    if cmd == "output":
+        sudo2.output()
+    elif cmd == "output -h":
+        sudo2.output_h()
+    else:
+        print("That's not a valid command! If you need commands, please use \"help\"!")
